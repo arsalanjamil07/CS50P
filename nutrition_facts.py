17901d6a022f4a2667139823ee3a365e8f21c67d
Arsalan Jamil <arsalanjamil07@gmail.com>
2024-10-20 22:42:29 +0500

    Nutrition Facts
    Implement a program that prompts consumers users to input a fruit and then outputs the number of calories in one portion of that fruit.

--- nutrition_facts.py
+++ nutrition_facts.py
@@ -4,9 +4,9 @@ def main():
     print("Calories:",output)
 
 def calories(f):
-    list={"A":20,"B":20,"C":30}
-    for i in list:
+    dict={"A":20,"B":20,"C":30}
+    for i in dict:
         if f==i:
-            return list[i]
+            return dict[i]
           
 main()
