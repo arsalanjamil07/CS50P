084d28988cfe585ce2fe670323fe2cf92767df92
Arsalan Jamil <arsalanjamil07@gmail.com>
2025-09-28 17:22:23 +0500

    Update home_federal_savings_bank.py

--- home_federal_savings_bank.py
+++ home_federal_savings_bank.py
@@ -1,7 +1,7 @@
 greeting = input("Enter a greeting: ").strip().lower()
 if greeting.startswith("hello"):
     print("$0")
-elif greeting.startsWith("h"):
+elif greeting.startsWith("h"): # elif greeting[0] == "h":
     print("$20")
 else:
     print("$100")
